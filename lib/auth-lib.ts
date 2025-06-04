6b4230d5cac4457f351f409a41282ab829b0eb53
codify4 <kushta.joni@gmail.com>
2025-06-04 21:00:15 +0200

    feat: sign up fixed

--- lib/auth-lib.ts
+++ lib/auth-lib.ts
@@ -3,28 +3,32 @@ import * as QueryParams from "expo-auth-session/build/QueryParams";
 import * as WebBrowser from "expo-web-browser";
 import { supabase } from "@/lib/supabase";
 
-WebBrowser.maybeCompleteAuthSession(); // required for web only
-const redirectTo = makeRedirectUri({
-  scheme: 'workoutmate',
-  path: 'signin',
-  preferLocalhost: false,
-});
+WebBrowser.maybeCompleteAuthSession();
+
+const redirectTo = 'workoutmate://signin';
 
 export const createSessionFromUrl = async (url: string) => {
     const { params, errorCode } = QueryParams.getQueryParams(url);
-  
-    if (errorCode) throw new Error(errorCode);
+    
+    if (errorCode) {
+      throw new Error(errorCode);
+    }
+    
     const { access_token, refresh_token } = params;
-  
-    if (!access_token) return;
+    
+    if (!access_token) {
+      return;
+    }
   
     const { data, error } = await supabase.auth.setSession({
       access_token,
       refresh_token,
     });
-    if (error) throw error;
     
-    // The navigation will be handled by the auth state change in _layout.tsx
+    if (error) {
+        throw error;
+    }
+    
     return data.session;
 };
 
@@ -36,19 +40,27 @@ export const performOAuth = async () => {
         skipBrowserRedirect: true,
       },
     });
-    if (error) throw error;
-  
+    
+    if (error) {
+      throw error;
+    }
+    
     const res = await WebBrowser.openAuthSessionAsync(
       data?.url ?? "",
-      redirectTo
+      redirectTo,
+      {
+        showInRecents: false,
+      }
     );
-  
+    
     if (res.type === "success") {
       const { url } = res;
       await createSessionFromUrl(url);
+    } else {
+      console.log("Cancelled");
     }
 };
 
 export async function signOut() {
-    const { error } = await supabase.auth.signOut()
+  const { error } = await supabase.auth.signOut();
 }
--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,11 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
 // Opt out of package.json:exports feature for Supabase compatibility
 config.resolver.unstable_enablePackageExports = false;
+config.resolver.sourceExts.push('cjs'); 
 
 module.exports = withNativeWind(config, { input: "./global.css" });
